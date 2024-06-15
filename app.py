4e7764831856e695b55d3a7447bb1d5b9f9060d2
liujiacheng <624453893@qq.com>
2024-06-15 16:06:15 +0800

    适配功能

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from DrissionPage import ChromiumPage, ChromiumOptions
 
 app = Flask(__name__)
 base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+cur_page, cur_page2 = None, None
 
 
 @app.route('/')
@@ -46,11 +47,14 @@ def button():
 @app.route('/new_api', methods=['POST'])
 def new_api():
     request_data = request.get_json()
+    # button.html会把参数传到这里，用于找到控制的是哪个浏览器页面
     new_addr = str(request_data['url'])
+    # 解析出参数列表
     params = new_addr[new_addr.find('?') + 1:].split('&')
     params_dict = dict()
     for item in params:
         params_dict[item[:item.find('=')]] = item[item.find('=') + 1:]
+    # address为需控制的浏览器页面，select_name为平台名称，用于确定打开哪个平台
     encode_addr = params_dict.get('address')
     select_name = params_dict.get('select_name')
     select_name = base64.urlsafe_b64decode(select_name).decode('utf-8')
@@ -61,23 +65,34 @@ def new_api():
         if item.get('name') == select_name:
             cur_platform = item
             break
+    # 获取到当前平台的配置项，包含多个表
     cur_config_list = cur_platform.get('config_list')
 
-    data_input_config = load_config('data_input_config')
+    # 获取当前平台用户填写的数据，统一放到池子里
+    # 即使一个相同的值，出现在了不同平台中，那也只取对应平台的，只是可以配置相同的id，在设置的时候一样
+    data_input_config = load_data('data_input_config')
+    if select_name not in data_input_config.keys():
+        return {
+            'status': 'error'
+        }
     data_pool = dict()
-    for (item, value) in data_input_config.items():
+    for (item, value) in data_input_config[select_name].items():
         for pool in value:
             data_pool[pool.get('key')] = pool.get('value')
 
     page = ChromiumPage(addr_or_opts=base64.urlsafe_b64decode(encode_addr).decode('utf-8'))
     target_page_html = page.latest_tab.html
     cur_map = None
+    # 获取到当前表的map映射关系
     if cur_platform.get('title_tag') == "":
+        # 如果tag为空，说明是统计局，对该平台所有配置项循环
         for item in cur_config_list:
+            # 找到某个表的map
             if item.get('name') in target_page_html:
                 cur_map = item.get('map')
                 break
     else:
+        # 一定要判断当前页面的title中包含关键字
         new_table_head_ele = page.latest_tab.ele('@class=' + cur_platform.get('title_tag'))
         if new_table_head_ele:
             # 说明是税务局情况，需要特殊判断
@@ -86,6 +101,15 @@ def new_api():
                 if item.get('name') in table_head:
                     cur_map = item.get('map')
                     break
+    for key, value in cur_map.items():
+        find_key = list(value.keys())[0]
+        find_value = value[find_key]
+        if key in data_pool.keys():
+            cur_ele = page.latest_tab.ele(f'@{find_key}={find_value}')
+            if cur_ele:
+                cur_ele.clear(by_js=True)
+                cur_ele.input('', clear=True)
+                cur_ele.input(data_pool[key], clear=True)
     frames = page.latest_tab.get_frames()
     print("total" + str(len(frames)))
     for frame in frames:
@@ -139,16 +163,21 @@ def data():
     url = str(request_data['url'])
     select_name = str(request_data['select_name'])
     co = ChromiumOptions().auto_port()
-    page = ChromiumPage(co)
-    page.get(url)
-    page.set.window.max()
-    page2 = ChromiumPage(co)
-    page2.set.window.size(100, 200)
-    page2.set.window.location(500, 0)
+    global cur_page, cur_page2
+    if cur_page is not None:
+        cur_page.quit()
+    if cur_page2 is not None:
+        cur_page2.quit()
+    cur_page = ChromiumPage(co)
+    cur_page.get(url)
+    cur_page.set.window.max()
+    cur_page2 = ChromiumPage(co)
+    cur_page2.set.window.size(100, 200)
+    cur_page2.set.window.location(500, 0)
     encode_select_name = base64.urlsafe_b64encode(select_name.encode('utf-8')).decode('utf-8')
-    encode_address = base64.urlsafe_b64encode(page.address.encode('utf-8')).decode('utf-8')
-    encode_button_address = base64.urlsafe_b64encode(page2.address.encode('utf-8')).decode('utf-8')
-    page2.get(f'http://127.0.0.1:5000/button?select_name={encode_select_name}&address={encode_address}&button_addr={encode_button_address}')
+    encode_address = base64.urlsafe_b64encode(cur_page.address.encode('utf-8')).decode('utf-8')
+    encode_button_address = base64.urlsafe_b64encode(cur_page2.address.encode('utf-8')).decode('utf-8')
+    cur_page2.get(f'http://127.0.0.1:5000/button?select_name={encode_select_name}&address={encode_address}&button_addr={encode_button_address}')
     return {}
 
 
@@ -196,8 +225,10 @@ def close_progress():
     page.quit()
     page2 = ChromiumPage(addr_or_opts=base64.urlsafe_b64decode(button_addr).decode('utf-8'))
     page2.quit()
-    os.kill(os.getpid(), signal.SIGINT)
-    sys.exit()
+    global cur_page, cur_page2
+    cur_page = None
+    cur_page2 = None
+    return {}
 
 
 def load_data(table_name):
@@ -243,5 +274,5 @@ def open_browser():
 
 
 if __name__ == '__main__':
-    Timer(0.5, open_browser).start()
+    webbrowser.open_new('http://127.0.0.1:5000')
     app.run()
